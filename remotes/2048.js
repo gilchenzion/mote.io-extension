5bd7aabd836ebd479900006e9aa48ec47baac47e
Gil Chen-Zion <gc2466@columbia.edu>
2014-04-05 23:58:59 -0400

    basics working

--- remotes/2048.js
+++ remotes/2048.js
@@ -7,9 +7,7 @@ exec(function(){
     twitter: 'gilcz2',
     display_input: false,
     init: function() {
-        setTimeout(function(){
-            mote.io.remote = {};
-        }, 5000);
+        //this.game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
     },
     update: function(force) {},
     blocks: [
@@ -18,9 +16,19 @@ exec(function(){
             data: [
                 {
                     press: function() {
-                        var e = jQ.Event("keydown");
-                        e.which = 27; // # Some key code value
-                        $("input").trigger(e);
+                        this.game.inputManager.emit("move", 0)
+                    },
+                    icon: 'chevron-up',
+                    hash: 'up'
+                }
+            ]
+        }
+        ,{
+            type: 'buttons',
+            data: [
+                {
+                    press: function() {
+                        this.game.inputManager.emit("move", 3)
                     },
                     icon: 'chevron-left',
                     hash: 'left'
@@ -28,6 +36,7 @@ exec(function(){
                 {
                     press: function() {
                         //When press
+                        this.game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
                     },
                     icon: 'circle-blank',
                     hash: 'new'
@@ -35,9 +44,22 @@ exec(function(){
                 {
                     press: function() {
                         //When press
+                        this.game.inputManager.emit("move", 1)
                     },
                     icon: 'chevron-right',
                     hash: 'right'
+                }
+            ]
+        },
+        {
+            type: 'buttons',
+            data: [
+                {
+                    press: function() {
+                        this.game.inputManager.emit("move", 2)
+                    },
+                    icon: 'chevron-down',
+                    hash: 'down'
                 }
             ]
         }
